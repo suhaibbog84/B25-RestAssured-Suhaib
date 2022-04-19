02eff8ac78fb7e56520e8e955d52d1ff3668c790
Suhaib B <suhaib.bog84@yahoo.com>
2022-04-19 16:25:21 -0400

    day 2 API - verify response header

--- src/test/java/com/cydeo/day2/SpartanGetRequests.java
+++ src/test/java/com/cydeo/day2/SpartanGetRequests.java
@@ -4,6 +4,7 @@ import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class SpartanGetRequests {
@@ -39,6 +40,58 @@ public class SpartanGetRequests {
         //verify content type is application json
         Assertions.assertEquals("application/json", response.contentType());
 
-
     }
+
+     /*
+        Given accept header is application/json
+        When users send a get request to /api/spartans/3
+        Then status code must be 200
+        And Content type must be application/json
+        And json body should contain 'Fidole'
+     */
+
+    @Test
+    public void test2() {
+
+        Response response = RestAssured.given().accept(ContentType.JSON)
+                .when()
+                .get(url + "/api/spartans/3");
+
+        //Verify status code is 200
+        Assertions.assertEquals(200, response.getStatusCode());
+
+        //Verify content type is application/json
+        Assertions.assertEquals("application/json", response.getContentType());
+
+        //Verify Fidole exist in json body
+        Assertions.assertTrue(response.body().asString().contains("Fidole"));
+    }
+        /*
+        Given no headers provided
+        When Users send GET request to /api/hello
+        Then response status code should be 200
+        And Content type header should be “text/plain;charset=UTF-8”
+        And header should contain date
+        And Content-Length should be 17
+        And body should be “Hello from Sparta"
+        */
+
+        @DisplayName("GET request to /api/hello endpoint")
+        @Test
+        public void test3(){
+
+
+            Response response = RestAssured.when().get(url+"/api/hello");
+
+            response.prettyPrint();
+
+            //verify status code
+        Assertions.assertEquals(200, response.statusCode());
+
+        //verify content type
+            Assertions.assertEquals();
+        }
+
+
+
 }

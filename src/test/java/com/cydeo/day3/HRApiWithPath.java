e38ff01684a2088bf2ac02afbd732d400bbf1c69
Suhaib B <suhaib.bog84@yahoo.com>
2022-04-21 14:32:09 -0400

    day 3 API - HR Test with Path

---
+++ src/test/java/com/cydeo/day3/HRApiWithPath.java
@@ -0,0 +1,12 @@
+package com.cydeo.day3;
+
+import com.cydeo.utilities.HrTestBase;
+
+import static io.restassured.RestAssured.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class HRApiWithPath extends HrTestBase {
+
+
+
+}

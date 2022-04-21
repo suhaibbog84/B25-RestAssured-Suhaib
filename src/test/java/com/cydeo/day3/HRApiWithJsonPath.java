b21fa6902a59d900dda759a8042d4788941c0521
Suhaib B <suhaib.bog84@yahoo.com>
2022-04-21 16:37:39 -0400

    day 3 API - HR with Json Path

--- src/test/java/com/cydeo/day3/HRApiWithJsonPath.java
+++ src/test/java/com/cydeo/day3/HRApiWithJsonPath.java
@@ -37,6 +37,36 @@ public class HRApiWithJsonPath extends HrTestBase {
         }
 
         //get me all country names where their region id is 2
+        List<Object> countryListWithID2 = jsonPath.getList("items.findAll {it.region_id==2}.country_id");
+        System.out.println(countryListWithID2);
+    }
+
+    @DisplayName("GET request to /employees with param")
+    @Test
+    public void test2(){
+
+        Response response = given().queryParam("limit", 150)
+                .when().get("/employees");
+
+        //get jsonpath object
+        JsonPath jsonPath = response.jsonPath();
+
+        //get me all emails who is working as IT_PROG
+        List<String> emailList = jsonPath.getList("items.findAll {it.job_id==\"IT_PROG\"}.email");
+        System.out.println(emailList);
+
+        //get me first name of employees who is making more than 10000
+        List<String> nameList = jsonPath.getList("items.findAll {it.salary>10000}.first_name");
+        System.out.println(nameList);
+
+        //get me the first name of who is making the highest salary
+        String maxFirstName = jsonPath.getString("items.max {it.salary}.first_name");
+        String minFirstName = jsonPath.getString("items.min {it.salary}.first_name");
+        System.out.println(maxFirstName);
+        System.out.println(minFirstName);
+
+        System.out.println(response.path("items.max {it.salary}.first_name").toString());
+
 
     }
 }

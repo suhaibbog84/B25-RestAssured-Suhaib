a6f3b3aaa0fdb7ae55670e97a4617b4a9347847b
Suhaib B <suhaib.bog84@yahoo.com>
2022-04-21 12:16:25 -0400

    day 3 API - utilities HR Test Base

--- src/test/java/com/cydeo/utilities/HrTestBase.java
+++ src/test/java/com/cydeo/utilities/HrTestBase.java
@@ -1,10 +1,13 @@
 package com.cydeo.utilities;
 
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeAll;
+
 public class HrTestBase {
 
     //beforeAll is the same thing with beforeClass in testng
     @BeforeAll
     public static void init(){
-        RestAssured.baseURI ="http://44.201.121.105:1000/ords/hr";
+        RestAssured.baseURI ="http://54.235.2.232:1000/ords/hr";
     }
 }

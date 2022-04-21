fceac0efd1bb25f5a2a802a1e0126c4095622eff
Suhaib B <suhaib.bog84@yahoo.com>
2022-04-21 14:54:20 -0400

    day 3 API - Spartan Test with Path

--- src/test/java/com/cydeo/day3/SpartanTestWithPath.java
+++ src/test/java/com/cydeo/day3/SpartanTestWithPath.java
@@ -6,6 +6,8 @@ import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static io.restassured.RestAssured.*;
 import static org.junit.jupiter.api.Assertions.*;
 

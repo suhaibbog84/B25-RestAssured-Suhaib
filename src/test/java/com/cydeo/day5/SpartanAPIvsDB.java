4968d484e78cd992e00eb2ce2dc43c0f8ffa065c
Suhaib B <suhaib.bog84@yahoo.com>
2022-04-25 16:06:20 -0400

    day 5 API - Spartan API vs DB

--- src/test/java/com/cydeo/day5/SpartanAPIvsDB.java
+++ src/test/java/com/cydeo/day5/SpartanAPIvsDB.java
@@ -40,6 +40,10 @@ public class SpartanAPIvsDB extends SpartanTestBase {
 
         System.out.println(dbMap);
         //compare api vs db
+        assertThat(apiMap.get("id").toString(),is(dbMap.get("SPARTAN_ID").toString()));
+        assertThat(apiMap.get("name"),is(dbMap.get("NAME")));
+        assertThat(apiMap.get("gender"),is(dbMap.get("GENDER")));
+        assertThat(apiMap.get("phone").toString(),is(dbMap.get("PHONE").toString()));
 
     }
 

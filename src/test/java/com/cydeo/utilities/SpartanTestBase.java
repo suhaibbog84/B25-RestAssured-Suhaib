3b5730d24d3e3603fe988d4f909d3eba58711d84
Suhaib B <suhaib.bog84@yahoo.com>
2022-04-25 15:53:53 -0400

    day 5 API - Spartan Test Base

--- src/test/java/com/cydeo/utilities/SpartanTestBase.java
+++ src/test/java/com/cydeo/utilities/SpartanTestBase.java
@@ -1,13 +1,25 @@
 package com.cydeo.utilities;
 
 import io.restassured.RestAssured;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 
 public class SpartanTestBase {
 
-        //beforeAll is the same thing with beforeClass in testng
-        @BeforeAll
-        public static void init(){
-            RestAssured.baseURI ="http://54.235.2.232:8000";
-        }
+    //beforeAll is the same thing with beforeClass in testng
+    @BeforeAll
+    public static void init(){
+        RestAssured.baseURI ="http://54.235.2.232:8000";
+
+        String dbUrl ="jdbc:oracle:thin:@54.235.2.232:1521:XE";
+        String dbUsername ="SP";
+        String dbPassword = "SP";
+
+        DBUtils.createConnection(dbUrl,dbUsername,dbPassword);
+    }
+
+    @AfterAll
+    public static void close(){
+        DBUtils.destroy();
+    }
 }

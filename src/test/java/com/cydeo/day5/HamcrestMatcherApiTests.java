b9f156e02ba7993fac7df51a53ce3ade885fb65d
Suhaib B <suhaib.bog84@yahoo.com>
2022-04-25 11:29:51 -0400

    day 5 API - Hamcrest Matchers Tests

---
+++ src/test/java/com/cydeo/day5/HamcrestMatcherApiTests.java
@@ -0,0 +1,46 @@
+package com.cydeo.day5;
+
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static io.restassured.RestAssured.*;
+
+public class HamcrestMatcherApiTests {
+
+     /*
+       given accept type is Json
+       And path param id is 15
+       When user sends a get request to spartans/{id}
+       Then status code is 200
+       And content type is Json
+       And json data has following
+           "id": 15,
+           "name": "Meta",
+           "gender": "Female",
+           "phone": 1938695106
+        */
+     @DisplayName("One Spartan with Hamcrest and Chaining")
+     @Test
+     public void test1() {
+         given()
+                 .accept(ContentType.JSON)
+                 .and()
+                 .pathParam("id", 15)
+                 .when()
+                 .get("http://44.201.121.105:8000/api/spartans/{id}")
+                 .then()
+                 .statusCode(200)
+                 .and()
+                 .contentType("application/json")
+                 .and()
+                 .body("id", is(15),
+                         "name", is("Meta"),
+                         "gender", equalTo("Female"),
+                         "phone", is(1938695106));
+     }
+
+
+}

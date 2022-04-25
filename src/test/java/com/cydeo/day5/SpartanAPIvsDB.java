f8e01e68af8dc698c14bb7bcf032d710e330f154
Suhaib B <suhaib.bog84@yahoo.com>
2022-04-25 15:58:09 -0400

    day 5 API - Spartan API vs DB

---
+++ src/test/java/com/cydeo/day5/SpartanAPIvsDB.java
@@ -0,0 +1,47 @@
+package com.cydeo.day5;
+
+import com.cydeo.utilities.DBUtils;
+import com.cydeo.utilities.SpartanTestBase;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static io.restassured.RestAssured.*;
+
+public class SpartanAPIvsDB extends SpartanTestBase {
+
+    @DisplayName("Compare one spartan information api vs db")
+    @Test
+    public void test1() {
+        Response response = given().accept(ContentType.JSON)
+                .pathParam("id", 15)
+                .when()
+                .get("/api/spartans/{id}")
+                .then()
+                .statusCode(200).extract().response();
+
+        //how to convert json response to map
+        Map<String, Object> apiMap = response.as(Map.class);
+        System.out.println(apiMap.toString());
+
+        //we need to get information from database
+        //which db we will connect ? oracle so we added depedency
+        //we need connection String of spartan db
+        String query = "SELECT SPARTAN_ID,NAME,GENDER,PHONE\n" +
+                "FROM SPARTANS\n" +
+                "WHERE SPARTAN_ID = 15";
+
+        Map<String, Object> dbMap = DBUtils.getRowMap(query);
+
+        System.out.println(dbMap);
+        //compare api vs db
+
+    }
+
+
+}

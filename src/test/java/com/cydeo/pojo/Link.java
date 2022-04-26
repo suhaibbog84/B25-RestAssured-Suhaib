3707e3b245df8afbf449356e7dd8486a550ab14b
Suhaib B <suhaib.bog84@yahoo.com>
2022-04-26 14:35:06 -0400

    day6 - Spartan Pojo Link Class

--- src/test/java/com/cydeo/pojo/Link.java
+++ src/test/java/com/cydeo/pojo/Link.java
@@ -1,12 +1,12 @@
 package com.cydeo.pojo;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 @Getter
 @Setter
 @ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class Link {
 
     private String rel;

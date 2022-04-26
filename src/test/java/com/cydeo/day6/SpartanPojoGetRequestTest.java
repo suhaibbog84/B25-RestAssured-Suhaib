2729f139b255ec13d5568117428efac05e72432a
Suhaib B <suhaib.bog84@yahoo.com>
2022-04-26 13:04:41 -0400

    day6 - Spartan Pojo Get Request

--- src/test/java/com/cydeo/day6/SpartanPojoGetRequestTest.java
+++ src/test/java/com/cydeo/day6/SpartanPojoGetRequestTest.java
@@ -9,6 +9,8 @@ import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 import static io.restassured.RestAssured.*;

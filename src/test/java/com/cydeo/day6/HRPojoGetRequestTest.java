76b798339d684833678f492d7d9793931fc865ee
Suhaib B <suhaib.bog84@yahoo.com>
2022-04-26 12:59:45 -0400

    day 6 API - Pojo HR Pojo Get  Request

---
+++ src/test/java/com/cydeo/day6/HRPojoGetRequestTest.java
@@ -0,0 +1,29 @@
+package com.cydeo.day6;
+
+import com.cydeo.pojo.Region;
+import com.cydeo.utilities.HrTestBase;
+import io.restassured.path.json.JsonPath;
+import org.junit.jupiter.api.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static io.restassured.RestAssured.*;
+import javax.swing.plaf.synth.Region;
+
+public class HRPojoGetRequestTest extends HrTestBase {
+
+    @Test
+    public void test1(){
+        JsonPath jsonPath = get("/regions").then().statusCode(200).extract().jsonPath();
+
+
+        //we want to store second region under items in Region class object
+        Region region2 = jsonPath.getObject("items[1]", Region.class);
+
+        System.out.println("region2.getRegion_id() = " + region2.getRegion_id());
+        System.out.println(region2.getRegion_name());
+        System.out.println(region2.getLinks().get(0).getHref());
+
+    }
+
+
+}

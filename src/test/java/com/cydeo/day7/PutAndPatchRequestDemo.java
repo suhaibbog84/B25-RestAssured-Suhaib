83a3c1cc808c1238337b806709b546b156f2e5ed
Suhaib B <suhaib.bog84@yahoo.com>
2022-04-28 14:31:01 -0400

    day7 - Spartan Put and patch Request

---
+++ src/test/java/com/cydeo/day7/PutAndPatchRequestDemo.java
@@ -0,0 +1,58 @@
+package com.cydeo.day7;
+
+import com.cydeo.utilities.SpartanTestBase;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static io.restassured.RestAssured.*;
+
+public class PutAndPatchRequestDemo extends SpartanTestBase {
+
+    @DisplayName("PUT request to one spartan for update with Map")
+    @Test
+    public void test1(){
+        //just like post request we have different opitons to send json body, we will go with map
+        Map<String,Object> putRequestMap = new LinkedHashMap<>();
+        putRequestMap.put("gender","Male");
+        putRequestMap.put("name","Severus Snape");
+        putRequestMap.put("phone",8877445596l);
+
+        given().contentType(ContentType.JSON)
+                .body(putRequestMap).log().body()
+                .and().pathParam("id",85)
+                .when().put("/api/spartans/{id}")
+                .then().statusCode(204);
+
+        //HW
+        //send a GET request to id number and make sure fields are updated
+
+
+    }
+
+    @DisplayName("PATCH request to one spartan for update with Map")
+    @Test
+    public void test2(){
+        //just like post request we have different opitons to send json body, we will go with map
+        Map<String,Object> putRequestMap = new LinkedHashMap<>();
+
+        putRequestMap.put("name","Severus");
+
+        given().contentType(ContentType.JSON)
+                .body(putRequestMap).log().body()
+                .and().pathParam("id",85)
+                .when().patch("/api/spartans/{id}")
+                .then().statusCode(204);
+
+        //HW
+        //send a GET request to id number and make sure fields are updated
+
+    }
+
+
+}

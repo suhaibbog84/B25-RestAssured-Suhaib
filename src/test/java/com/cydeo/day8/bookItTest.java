33b0adc0c84769581cb5414c6efc7a1d24c50769
Suhaib B <suhaib.bog84@yahoo.com>
2022-04-29 11:13:24 -0400

    day8 - BookIt Test

---
+++ src/test/java/com/cydeo/day8/bookItTest.java
@@ -0,0 +1,11 @@
+package com.cydeo.day8;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static io.restassured.RestAssured.*;
+
+public class bookItTest {
+
+
+
+}

561d2a467c7f8e97b9a891603d895cd37aa919ab
Suhaib B <suhaib.bog84@yahoo.com>
2022-04-29 11:32:41 -0400

    day8 - BookIt Test

--- src/test/java/com/cydeo/day8/bookItTest.java
+++ src/test/java/com/cydeo/day8/bookItTest.java
@@ -1,11 +1,55 @@
 package com.cydeo.day8;
 
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 import static io.restassured.RestAssured.*;
 
 public class bookItTest {
 
+    @BeforeAll
+    public static void init(){
+        RestAssured.baseURI = "https://cybertek-reservation-api-qa.herokuapp.com";
+
+    }
+
+    String accessToken = "eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiIxMTMxMiIsImF1ZCI6InN0dWRlbnQtdGVhbS1sZWFkZXIifQ.ZIHJuDh19eLga3bLP7udnvNtEA0DM_W1H67ah2Zu3Lc";
+
+    @DisplayName("GET /api/campuses")
+    @Test
+    public void test1(){
+
+        //how to pass bearer token for bookit?
+        given()
+                .header("Authorization",accessToken)
+                .accept(ContentType.JSON)
+        .when()
+                .get("/api/campuses")
+                .then().statusCode(200)
+                .log().all();
+    }
+
+    @DisplayName("GET /api/users/me")
+    @Test
+    public void test2(){
+        given()
+                .header("Authorization",accessToken)
+                .accept(ContentType.JSON)
+                .when()
+                .get("/api/users/me")
+                .then().statusCode(200)
+                .log().all();
+
+
+    }
+
+    //create bookitUtil class
+    //create static method that accepts email and password, and return token with "Bearer your-token"
 
 
 }

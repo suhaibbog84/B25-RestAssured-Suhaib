e164472be287e804bf0930643e703d7f010c812d
Suhaib B <suhaib.bog84@yahoo.com>
2022-05-05 12:35:33 -0400

    day10 - Response Time Test

---
+++ src/test/java/com/cydeo/day10/ResponseTimeTest.java
@@ -0,0 +1,19 @@
+package com.cydeo.day10;
+
+import com.cydeo.utilities.SpartanWithAuthTestBase;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static io.restassured.RestAssured.*;
+
+public class ResponseTimeTest extends SpartanWithAuthTestBase {
+
+    @DisplayName("GET request to all spartans and verify response time")
+    @Test
+    public void test1(){
+
+    }
+
+}

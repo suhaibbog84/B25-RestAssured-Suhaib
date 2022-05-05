7214f0b5fba65b30d45f9bb0be1eec84bf85ba4e
Suhaib B <suhaib.bog84@yahoo.com>
2022-05-05 12:43:18 -0400

    day10 - Response Time Test

--- src/test/java/com/cydeo/day10/ResponseTimeTest.java
+++ src/test/java/com/cydeo/day10/ResponseTimeTest.java
@@ -1,6 +1,8 @@
 package com.cydeo.day10;
 
 import com.cydeo.utilities.SpartanWithAuthTestBase;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -14,6 +16,17 @@ public class ResponseTimeTest extends SpartanWithAuthTestBase {
     @Test
     public void test1(){
 
+      Response response = given()
+                .accept(ContentType.JSON)
+                .and()
+                .auth().basic("admin", "admin")
+                .when().get("api/spartans")
+                .then().statusCode(200)
+              .and()
+              .time(lessThanOrEqualTo(1300L))
+              .extract().response();
+
+        System.out.println("response.getTime() = " + response.getTime());
     }
 
 }

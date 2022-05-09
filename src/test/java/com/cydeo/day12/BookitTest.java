5d4e92b011688d852daf36d9dee0e2b8c81d2694
Suhaib B <suhaib.bog84@yahoo.com>
2022-05-09 12:29:04 -0400

    day12 - BookitQa3 Excel File

--- src/test/java/com/cydeo/day12/BookitTest.java
+++ src/test/java/com/cydeo/day12/BookitTest.java
@@ -1,19 +1,41 @@
 package com.cydeo.day12;
 
 import com.cydeo.utilities.ExcelUtil;
+import io.restassured.http.ContentType;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static io.restassured.RestAssured.*;
 
 import java.util.List;
 import java.util.Map;
 
 public class BookitTest {
 
+    //create a method to read bookitqa3 excel file information
+    //use those information as an email and password to send a get request to /sign endpoint
+    //verify you got 200 for each request
+    //print accesstoken for each request
+    //https://cybertek-reservation-api-qa3.herokuapp.com
+
     @ParameterizedTest
     @MethodSource("getBookitData")
-    public void excelParamTest(Map<String,String> userInfo){
+    public void excelBookitTest(Map<String,String> userInfo){
         System.out.println("userInfo.get(\"email\") = " + userInfo.get("email"));
         System.out.println("userInfo.get(\"password\") = " + userInfo.get("password"));
+
+        given()
+                .baseUri("https://cybertek-reservation-api-qa3.herokuapp.com")
+                .and()
+                .accept(ContentType.JSON)
+                .and()
+                .queryParams(userInfo)
+        .when()
+                .get("/sign")
+        .then()
+                .statusCode(200).log().all();
 
     }
     public static List<Map<String,String>> getBookitData(){

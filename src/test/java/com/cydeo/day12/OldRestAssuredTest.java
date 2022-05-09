710fc4bf4db7bfcfc35099116fb646d805e73991
Suhaib B <suhaib.bog84@yahoo.com>
2022-05-09 12:53:11 -0400

    day12 - Old Rest Assured Test

--- src/test/java/com/cydeo/day12/OldRestAssuredTest.java
+++ src/test/java/com/cydeo/day12/OldRestAssuredTest.java
@@ -1,10 +1,59 @@
 package com.cydeo.day12;
 
 import com.cydeo.utilities.SpartanNewBase;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static io.restassured.RestAssured.*;
 
 public class OldRestAssuredTest extends SpartanNewBase {
 
+    @Test
+    public void getAllSpartan(){
+        given()
+                .accept(ContentType.JSON)
+                .and()
+                .auth().basic("admin","admin")
+                .log().all()
+                .when()
+                .get("/spartans")
+                .then()
+                .statusCode(200)
+                .and()
+                .contentType(ContentType.JSON)
+                .and()
+                .body("id[0]",is(10))
+                .body("id[1]",is(150))
+                .log().all();
+        //Hard assertion, it will not continue if the test failed
+    }
 
+    /*
+        in previous version of RestAssured, the given when then style
+        was actually written in given expect when format.
+        it will assert all the result and give one answer and does not fail while thing
+        if first one fail unlike new structure.
+     */
+    @Test
+    public void getSpartanOldWay(){
+        given()
+                .accept(ContentType.JSON)
+                .and()
+                .auth().basic("admin","admin")
+                .log().all()
+                .expect()
+                .statusCode(200)
+                .and()
+                .contentType(ContentType.JSON)
+                .body("id[0]",is(10))
+                .body("id[1]",is(150))
+                .logDetail(LogDetail.ALL)
+                .when()
+                .get("/spartans");
+        //soft assertion that will continue the code even if the test failed
+    }
 
 
 }

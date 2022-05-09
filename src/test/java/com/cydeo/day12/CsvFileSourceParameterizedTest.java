4ad23b39c26dfe4b247fefc0f3204c40c489fe57
Suhaib B <suhaib.bog84@yahoo.com>
2022-05-09 10:31:09 -0400

    day12 - Csv Source File Parameterized Test

---
+++ src/test/java/com/cydeo/day12/CsvFileSourceParameterizedTest.java
@@ -0,0 +1,8 @@
+package com.cydeo.day12;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static io.restassured.RestAssured.*;
+
+public class CsvFileSourceParameterizedTest {
+}

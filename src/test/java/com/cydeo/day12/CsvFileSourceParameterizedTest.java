9d8e9ace79651dc9427f942aedcaeda2337bde0f
Suhaib B <suhaib.bog84@yahoo.com>
2022-05-09 10:38:28 -0400

    day12 - Csv Source File Parameterized Test

--- src/test/java/com/cydeo/day12/CsvFileSourceParameterizedTest.java
+++ src/test/java/com/cydeo/day12/CsvFileSourceParameterizedTest.java
@@ -1,8 +1,22 @@
 package com.cydeo.day12;
 
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 import static io.restassured.RestAssured.*;
 
 public class CsvFileSourceParameterizedTest {
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/postalcode.csv",numLinesToSkip = 1)
+    public void zipCodeTestWithFile(String state, String city, int zipCount){
+
+        System.out.println("state = " + state);
+        System.out.println("city = " + city);
+        System.out.println("zipCount = " + zipCount);
+
+
+    }
 }

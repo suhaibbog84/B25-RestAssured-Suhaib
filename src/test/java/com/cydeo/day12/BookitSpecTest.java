c2999c1ac000137b8ab4aa56cee1b637e496eed9
Suhaib B <suhaib.bog84@yahoo.com>
2022-05-09 14:42:06 -0400

    day12 - Bookit Spec Test

---
+++ src/test/java/com/cydeo/day12/BookitSpecTest.java
@@ -0,0 +1,12 @@
+package com.cydeo.day12;
+
+import org.junit.jupiter.api.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static io.restassured.RestAssured.*;
+
+public class BookitSpecTest {
+
+    @Test
+
+}

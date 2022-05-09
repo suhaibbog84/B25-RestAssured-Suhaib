9801c2af8efdb5fa0854831ea79f15b3e00209a4
Suhaib B <suhaib.bog84@yahoo.com>
2022-05-09 15:45:38 -0400

    day12 - Bppkit Test Base

--- src/test/java/com/cydeo/day12/BookitSpecTest.java
+++ src/test/java/com/cydeo/day12/BookitSpecTest.java
@@ -19,71 +19,30 @@ import static io.restassured.RestAssured.*;
 
 public class BookitSpecTest extends BookitTestBase {
 
-    public static RequestSpecification teacherReqSpec;
-    public static ResponseSpecification responseSpec;
-    public static RequestSpecification studentMemberReqSpec;
-    @BeforeAll
-    public static void init(){
-        baseURI = ConfigurationReader.getProperty("base_url");
+    @Test
+    public void test1(){
+        //send a get request to /api/users/me endpoint as a teacher
+        //verify status code and content type
+        given()
+                .spec(teacherReqSpec)
+                .when()
+                .get("/api/users/me")
+                .then()
+                .spec(responseSpec);
 
-        teacherReqSpec = given()
-                .header("Authorization",getTokenByRole("teacher"))
-                .accept(ContentType.JSON)
-                .log().all();
-
-        studentMemberReqSpec = given()
-                .header("Authorization",getTokenByRole("student-member"))
-                .accept(ContentType.JSON)
-                .log().all();
-
-
-        responseSpec = expect().statusCode(200)
-                .and()
-                .contentType(ContentType.JSON)
-                .logDetail(LogDetail.ALL);
-    }
-    //create a method that returns Bearer+token based on provided role
-    //teacher,student-member,student-leader
-    //it will take info from configuration properties
-    public static String getTokenByRole(String role) {
-        String email = "";
-        String password = "";
-
-        switch (role) {
-            case "teacher":
-                email = ConfigurationReader.getProperty("teacher_email");
-                password = ConfigurationReader.getProperty("teacher_password");
-                break;
-
-            case "student-member":
-                email = ConfigurationReader.getProperty("team_member_email");
-                password = ConfigurationReader.getProperty("team_member_password");
-                break;
-            case "student-leader":
-                email = ConfigurationReader.getProperty("team_leader_email");
-                password = ConfigurationReader.getProperty("team_leader_password");
-                break;
-            default:
-
-                throw new RuntimeException("Invalid Role Entry :\n>> " + role +" <<");
-        }
-
-        Map<String, String> credentials = new HashMap<>();
-        credentials.put("email", email);
-        credentials.put("password", password);
-
-        String path = given()
-                .queryParams(credentials)
-                .when().get( "/sign").path("accessToken");
-
-        return  "Bearer " + path;
 
     }
 
-    @AfterAll
-    public static void close(){
-        reset();
+    @Test
+    public void test2(){
+        //send a get request to /api/users/me as a student-member
+        //verify status code and content type
+        given()
+                .spec(userReqSpec("student-member"))
+                .when().get("/api/users/me")
+                .then()
+                .spec(dynamicResponseSpec(200));
+
+
+
     }
-
-
-}
--- src/test/java/com/cydeo/utilities/BookitTestBase.java
+++ src/test/java/com/cydeo/utilities/BookitTestBase.java
@@ -1,43 +1,102 @@
 package com.cydeo.utilities;
 
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+
 import java.util.HashMap;
 import java.util.Map;
 
-import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.*;
 
 public class BookitTestBase {
 
-    public static String generateToken(String role){
-        Map<String, String> credentials = new HashMap<>();
+    public static RequestSpecification teacherReqSpec;
+    public static ResponseSpecification responseSpec;
+    public static RequestSpecification studentMemberReqSpec;
+    @BeforeAll
+    public static void init(){
+        baseURI = ConfigurationReader.getProperty("base_url");
 
-        switch (role){
+        teacherReqSpec = given()
+                .header("Authorization",getTokenByRole("teacher"))
+                .accept(ContentType.JSON)
+                .log().all();
 
+        studentMemberReqSpec = given()
+                .header("Authorization",getTokenByRole("student-member"))
+                .accept(ContentType.JSON)
+                .log().all();
+
+
+        responseSpec = expect().statusCode(200)
+                .and()
+                .contentType(ContentType.JSON)
+                .logDetail(LogDetail.ALL);
+    }
+
+    //can we create a method that is returning RequestSpec based on the role we provided ?
+    public static RequestSpecification userReqSpec(String role){
+
+        return given()
+                .header("Authorization",getTokenByRole(role))
+                .accept(ContentType.JSON)
+                .log().all();
+
+    }
+
+    public static ResponseSpecification dynamicResponseSpec(int statusCode){
+        return expect().statusCode(statusCode)
+                .and()
+                .contentType(ContentType.JSON)
+                .logDetail(LogDetail.ALL);
+
+    }
+
+    //create a method that returns Bearer+token based on provided role
+    //teacher,student-member,student-leader
+    //it will take info from configuration properties
+    public static String getTokenByRole(String role) {
+        String email = "";
+        String password = "";
+
+        switch (role) {
             case "teacher":
-                credentials.put("email", ConfigurationReader.getProperty("teacher_email"));
-                credentials.put("password",ConfigurationReader.getProperty("teacher_password"));
-                break;
-
-            case "team-leader":
-                credentials.put("email",ConfigurationReader.getProperty("team_leader_email"));
-                credentials.put("password",ConfigurationReader.getProperty("team_leader_password"));
+                email = ConfigurationReader.getProperty("teacher_email");
+                password = ConfigurationReader.getProperty("teacher_password");
                 break;
 
             case "student-member":
-                credentials.put("email",ConfigurationReader.getProperty("team_member_email"));
-                credentials.put("password",ConfigurationReader.getProperty("team_member_password"));
+                email = ConfigurationReader.getProperty("team_member_email");
+                password = ConfigurationReader.getProperty("team_member_password");
                 break;
+            case "student-leader":
+                email = ConfigurationReader.getProperty("team_leader_email");
+                password = ConfigurationReader.getProperty("team_leader_password");
+                break;
+            default:
 
+                throw new RuntimeException("Invalid Role Entry :\n>> " + role +" <<");
         }
 
-        String accessToken = given().log().uri()
-                .queryParams(credentials)
-                .when()
-                .get("/sign").
-                then()
-                .statusCode(200)
-                .extract().jsonPath().getString("accessToken");
+        Map<String, String> credentials = new HashMap<>();
+        credentials.put("email", email);
+        credentials.put("password", password);
 
-        return "Bearer "+accessToken;
+        String path = given()
+                .queryParams(credentials)
+                .when().get( "/sign").path("accessToken");
+
+        return  "Bearer " + path;
+
+    }
+
+    @AfterAll
+    public static void close(){
+        reset();
     }
 
 }

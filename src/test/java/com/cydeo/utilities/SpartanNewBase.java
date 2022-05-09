e1fb7e904343aade11bf07082d0dbf228bbb35a5
Suhaib B <suhaib.bog84@yahoo.com>
2022-05-09 14:32:57 -0400

    day12 - Spartan New Base

--- src/test/java/com/cydeo/utilities/SpartanNewBase.java
+++ src/test/java/com/cydeo/utilities/SpartanNewBase.java
@@ -1,5 +1,7 @@
 package com.cydeo.utilities;
 
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.BeforeAll;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.AfterAll;
@@ -7,20 +9,39 @@ import static io.restassured.RestAssured.*;
 
 public class SpartanNewBase {
 
+    public static  RequestSpecification reqSpec;
+    public static ResponseSpecification responseSpec;
+    public static RequestSpecification userSpec;
+    public static RequestSpecification adminSpec;
     @BeforeAll
-    public static void init() {
-
-        baseURI = "http://44.201.121.105";
+    public static void init(){
+        baseURI ="http://44.201.121.105";
         port = 7000;
-        basePath = "/api";
+        basePath ="/api";
+
+        reqSpec = given()
+                .accept(ContentType.JSON)
+                .and()
+                .auth().basic("admin", "admin")
+                .log().all();
+
+        userSpec = given()
+                .accept(ContentType.JSON)
+                .and()
+                .auth().basic("user", "user")
+                .log().all();
+
+        responseSpec = expect().statusCode(200)
+                .and()
+                .contentType(ContentType.JSON)
+                .logDetail(LogDetail.ALL);
     }
 
-        @AfterAll
-         public static void close(){
-            //reste the info we set above, method comes from restassured
-            reset();
-
-        }
+    @AfterAll
+    public static void close(){
+        //reste the info we set above, method comes from restassured
+        reset();
+    }
 
 
 }
